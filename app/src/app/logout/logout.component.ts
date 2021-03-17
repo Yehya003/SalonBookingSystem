a33d87e4f3cf47cc01809888891402bbcac8da88
ahmedElkady01 <49401216+ahmedElkady01@users.noreply.github.com>
2021-03-17 12:00:32 +0100

    Added the log out

--- app/src/app/logout/logout.component.ts
+++ app/src/app/logout/logout.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
+  styleUrls: ['./logout.component.css'],
 })
 export class LogoutComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  logoutUser() {
+    localStorage.removeItem('token');
+      this.router.navigate(['/homePage']);
   }
-
 }
